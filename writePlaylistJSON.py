723c84d3e699a89f10d69a6b95cfce6c0331c917
IllimitedandCompany <127307334+IllimitedandCompany@users.noreply.github.com>
2023-09-16 17:02:00 +0200

    Create writePlaylistJSON.py

---
+++ writePlaylistJSON.py
@@ -0,0 +1,22 @@
+import os
+import json
+
+# Directory path where your music files are located
+music_dir = 'path/to/your/music/folder'
+
+# Get a list of music file names in the directory
+music_files = [file for file in os.listdir(music_dir) if file.endswith(('.mp3', '.wav', '.flac'))]
+
+# Create a dictionary to store the music file names
+music_data = {
+    'allMusics': music_files
+}
+
+# Define the path for the JSON file
+json_file_path = 'path/to/your/output/json/file.json'
+
+# Write the data to the JSON file
+with open(json_file_path, 'w') as json_file:
+    json.dump(music_data, json_file, indent=4)
+
+print(f"JSON file with music names saved at {json_file_path}")
